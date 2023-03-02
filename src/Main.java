0e68731402b4ad64bc7fd9e4fc13f1547c0e7a86
Mert Bayats <mertbaytas@gmail.com>
2023-03-02 09:55:48 +0300

    sa

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         float derece;
-        System.out.print("Derece Giriniz : ");
+        System.out.print("Derece Giriniz (Celcius): ");
         Scanner oku=new Scanner(System.in);
         derece=oku.nextFloat();
 
         if(derece<5){
             System.out.println("Buzz! Kayak havasi");
         } else if (derece<=15 && derece>5) {
-            System.out.println("Biraz soguk ama avm sicaktir. Sinema Havasi");
+            System.out.println("Biraz soguk. Avm de sinema Havasi");
         } else if (derece>15 && derece<=25) {
             System.out.println("Hava serbet gibi, Piknik vakti");
         } else {
